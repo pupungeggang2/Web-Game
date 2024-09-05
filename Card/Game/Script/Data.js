bf59abc7c2a2f070a095710b8a67d585ef680417
pupungeggang2 <kyongkyong2892@gmail.com>
2024-09-05 17:04:01 +0900

    0.0.0

---
+++ Card/Game/Script/Data.js
@@ -0,0 +1,11 @@
+const dataField = {
+
+}
+
+const dataCard = {
+
+}
+
+const dataMonster = {
+    
+}
---
+++ Card/Game/Script/FunctionSave.js
@@ -0,0 +1,19 @@
+function saveInit() {
+    let tempSave = localStorage.getItem(fileName)
+
+    if (tempSave == null) {
+        
+    }
+}
+
+function saveData() {
+
+}
+
+function loadData() {
+
+}
+
+function eraseData() {
+
+}
