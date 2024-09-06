f4a7b9735524e3b4414ca2b6751bf28bac528116
pupungeggang6 <pupungeggang6@gmail.com>
2024-09-07 00:43:15 +0900

    Start

--- CardReal/Game/Script/SceneTitle.js
+++ CardReal/Game/Script/SceneTitle.js
@@ -5,7 +5,13 @@ function loopTitle() {
 function displayTitle() {
     drawSceneInit()
 
-    context.fillText('Start', 8, 8)
+    context.fillText('Desserterria', UI.title.textTitle[0], UI.title.textTitle[1])
+    context.strokeRect(UI.title.buttonStart[0], UI.title.buttonStart[1], UI.title.buttonStart[2], UI.title.buttonStart[3])
+    context.fillText('Start Game', UI.title.textStart[0], UI.title.textStart[1 ])
+    context.strokeRect(UI.title.buttonOption[0], UI.title.buttonOption[1], UI.title.buttonOption[2], UI.title.buttonOption[3])
+    context.fillText('Option', UI.title.textOption[0], UI.title.textOption[1])
+    context.strokeRect(UI.title.buttonErase[0], UI.title.buttonErase[1], UI.title.buttonErase[2], UI.title.buttonErase[3])
+    context.fillText('Erase', UI.title.textErase[0], UI.title.textErase[1])
 }
 
 function mouseUpTitle(x, y, button) {
---
+++ CardReal/Game/Script/UI.js
@@ -0,0 +1,11 @@
+const UI = {
+    title : {
+        textTitle : [8, 8],
+        buttonStart : [160, 160, 960, 80],
+        textStart : [168, 184],
+        buttonOption : [160, 240, 960, 80],
+        textOption : [168, 264],
+        buttonErase : [160, 320, 960, 80],
+        textErase : [168, 344],
+    }
+}
