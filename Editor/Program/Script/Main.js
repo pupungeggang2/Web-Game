db5c7e57292a82f6b336f2f51a57425b413d5ed4
pupungeggang6 <pupungeggang6@gmail.com>
2024-09-10 14:45:18 +0900

    Temp

---
+++ Editor/Program/Script/Main.js
@@ -0,0 +1,41 @@
+window.onload = main
+window.onerror = errorHandle
+window.oncontextmenu = rightClick
+
+const promiseLoad = new Promise((resolve, reject) => {
+    try {
+        imageLoad()
+        resolve('Success')
+    } catch {
+        reject('Fail')
+    }
+})
+
+function main() {
+    promiseLoad.then(() => {
+        canvas = document.getElementById('Screen')
+        context = cavnas.getContext('2d')
+
+        window.addEventListener('mouseup', mouseUp, false)
+        window.addEventListener('keyDown', keyDown, false)
+        window.addEventListener('keyup', keyUp, false)
+
+        programFrameCurrent = Date.now()
+        programFramePrevious = Date.now() - 16
+        programInstance = requestAnimationFrame(loop)
+    })
+}
+
+function loop() {
+    programInstance = requestAnimationFrame(loop)
+}
+
+function errorHandle(err, url, line, col, obj) {
+    if (obj != null) {
+        cancelAnimationFrame(programInstance)
+    }
+}
+
+function rightClick() {
+    return false
+}
---
+++ Editor/Program/Script/Var.js
@@ -0,0 +1,18 @@
+let canvas
+let context
+
+let programInstance
+let programFrameCurrent
+let programFramePrevious
+let delta
+
+let scene = 'Edit'
+let state = ''
+
+let varEditor = {
+
+}
+
+let varTest = {
+
+}
